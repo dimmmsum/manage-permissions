9dc2d303ee931d3689da1256541365922e7ae6f7
dimmmsum <140883604+dimmmsum@users.noreply.github.com>
2026-01-15 10:39:41 +0700

    Change database name from 'db_order' to 'db'

--- manage_permissions.php
+++ manage_permissions.php
@@ -48,7 +48,7 @@
         $servername = "localhost";
         $db_username = "root";
         $db_password = "";
-        $dbname = "db_order";
+        $dbname = "db";
         $conn = new mysqli($servername, $db_username, $db_password, $dbname);
 
         if ($conn->connect_error) {
